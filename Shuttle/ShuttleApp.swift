85d0caab5dffd12d5d4620911fbed6668274e2a2
NanditaAggarwal <naggarwal@bbns.org>
2023-05-09 21:28:56 -0400

    Initial Commit

---
+++ Shuttle/ShuttleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuttleApp.swift
+//  Shuttle
+//
+//  Created by Nandita Aggarwal on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ShuttleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
